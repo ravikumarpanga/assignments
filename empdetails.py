bdad07c00d4a170da3a07d585d9fecaf4174e163
ravikumarpanga <140778297+ravikumarpanga@users.noreply.github.com>
2023-09-19 16:12:36 +0530

    Add files via upload

--- empdetails.py
+++ empdetails.py
@@ -1,12 +1,21 @@
-def empdetails():
-    s={}
-    name=input("enter a empname:")
-    domain=input("enter a domain:")
-    empid=input("enter a empid:")
-    email=input("enter a email:")
-    s["name"]=name
-    s["domain"]=domain
-    s["empid"]=empid
-    s["mail"]=email
-    print(s)
-empdetails()
+def add_emp():
+    empdata = {}
+    n=int(input("enter no.of emp:"))    
+    for i in range(1,n+1):   
+        domain = input("Enter the emp domain: ")
+        name = input("Enter the emp name: ")
+        empid = input("Enter the emp empid: ")
+        email = input("Enter the emp email: ")
+        empdetails = {"Domain": domain,"Name": name,"Email": email}
+        empdata[empid] = empdetails
+        print("Employee details added successfully!")
+    print()
+    domain = input("Enter which domain details do you want: ")
+    found = False
+    for key, value in empdata.items():
+        if(value["Domain"] == domain):
+            print(f"{key}: {value}")
+            found = True
+    if not found:
+        print("No employees found with the specified domain.") 
+add_emp()
