3250fdcc3d55dbc6bc737fefb6ab1fffbb485837
ravikumarpanga <140778297+ravikumarpanga@users.noreply.github.com>
2023-10-03 12:13:03 +0530

    Add files via upload

---
+++ employeedetails.py
@@ -0,0 +1,19 @@
+#Write a program to enter employee details and also filter  the stored employee  details  with name and empid and designation and email. 
+# employees={}
+# n=int(input("num of emp:"))
+
+# for i in range(n):
+#     name=input("name of the employee:")
+#     empid=input("email id:")
+#     destination=input("emp destination:")
+#     email=input("emp email id")
+#     employees[name]={"empid":empid,"destination":destination,"email":email}
+#     print("employee details added successfully")
+# def filter_details():
+#     wanted_destination=input("enter destination:")
+#     for key,value in employees.items():
+#         if value["destination"]==wanted_destination:
+#             print(key,value)
+
+# filter_details()
+
