d209c64b1b6a2cb42936020db3b05123a836a630
ravikumarpanga <140778297+ravikumarpanga@users.noreply.github.com>
2023-10-16 17:38:31 +0530

    Update shoppingcart.py

--- shoppingcart.py
+++ shoppingcart.py
@@ -12,9 +12,85 @@
 #                 cart[self.Item]=self.price
 #                 Total_amount+=self.price
 #                 number_of_items+=1
+#         print(cart)
+#         print(f"Total amount={Total_amount}")
+#         print(f"Total number of items={number_of_items}")
+#         remove_item=input("you want to delete any item(yes or no):")
+#         if remove_item=="yes":
+#             cart.pop(input("Enter a deleting item:"))
+#             number_of_items-=1
+#             Total_amount-=self.price
+#             print(cart)
+#             print(f"Total amount={Total_amount}")
+#             print(f"Total number of items={number_of_items}")
+#         else:
+#             print("no changes")
                 
 #         print(cart)
 #         print(f"Total amount={Total_amount}")
 #         print(f"Total number of items={number_of_items}")
 # s=flipkart() 
+
+
+class flipkartShopping:
+    def _init_(self):
+        self.items_list = []
+        self.prices = []
+        self.menu()
+    
+    def menu(self):
+        while True:
+            print("Welcome to Shopping Cart Program!")
+            print()
+            print("Please select one of the following:\n" "1. Add Items\n" "2. View Cart\n" "3. Remove Items\n" "4. Compute Total\n" "5. quit\n")           
+            action = input("Please enter the action: ")
+            print()          
+            if(action == "1"):
+                self.add_items()
+            elif(action == "2"):
+                self.view_cart()
+            elif(action == "3"):
+                self.remove_items()
+            elif(action == "4"):
+                self.compute_total()
+            elif(action == "5"):
+                self.quit()
+            else:
+                print("select any one of the following options")
+            
+    def add_items(self):
+        Item = input("What item would you like to add: ")
+        self.items_list.append(Item)
+        price = float(input(f"what is the price of '{Item.capitalize()}': "))
+        self.prices.append(price)
+        
+        print(f"'{Item.capitalize()}' has been added to the cart Successfully.\n")
+        
+    def view_cart(self):
+        print("The Items in your Cart are: \n")
+        for i in range(len(self.items_list)) and range(len(self.prices)):
+            item = self.items_list[i]
+            price = self.prices[i]
+            items = (f'{i+1}. {item.capitalize()} - {float(self.prices[i])}')
+            print(items)
+            print()
+    
+    def remove_items(self):
+        index = int(input("What item would you like to remove: "))
+        self.items_list.pop(index-1)
+        self.prices.pop(index-1)
+        print("The selected item removed successfully")
+        
+    def compute_total(self):
+        total = 0
+        for i in range(len(self.items_list)):
+            total += float(self.prices[i])
+        print(f"The total price of the items in the shopping cart is {total}")
+               
+    def quit(self):
+        print("Please visit us Again and spent some money here!")  
+        print()
+            
+    
+flipkartShopping()
 
